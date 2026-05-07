39c98500c5adb220b5521bfb814b5bff2abd47b8
문정훈 <mjh090801abc@gmail.com>
2026-05-07 11:57:29 +0900

    App파일에 import하여 적용

--- chapter5/src/App.js
+++ chapter5/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './component/Footer';
 import Header from './component/Header';
+import Body from './component/body';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Body />
+      <Footer />
     </div>
 
     // <div></div> === <div />
