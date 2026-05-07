26e9aabb137f697583fb79dcf0702a4ffd802b82
문정훈 <mjh090801abc@gmail.com>
2026-05-07 18:05:18 +0900

    삼항 연산자를 사용하여 값 반환

--- chapter5/src/component/body.js
+++ chapter5/src/component/body.js
@@ -1,13 +1,15 @@
 function Body() {
-    const obj = {
-        sc: "상철",
-        yj: "용준"
-    }
+    // const obj = {
+    //     sc: "상철",
+    //     yj: "용준"
+    // }
+
+    const number = 10;
+
     return(
         <div>
             <h1>Body</h1>
-            <h2>{obj.sc}</h2>
-            <h2>{obj.yj}</h2>
+            <h2>{number}은 {number % 2 === 0 ? "짝수" : "홀수"}입니다잉</h2>
         </div>
     );
 }
