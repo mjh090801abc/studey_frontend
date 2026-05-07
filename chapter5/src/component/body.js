49f4e22797e550a8b93b930bf12f3e2364c05f5e
문정훈 <mjh090801abc@gmail.com>
2026-05-07 16:08:08 +0900

    배열로 값을 반환

--- chapter5/src/component/body.js
+++ chapter5/src/component/body.js
@@ -1,9 +1,13 @@
 function Body() {
-    const number = 10
+    const obj = {
+        sc: "상철",
+        yj: "용준"
+    }
     return(
         <div>
             <h1>Body</h1>
-            <h1>{number}</h1>
+            <h2>{obj.sc}</h2>
+            <h2>{obj.yj}</h2>
         </div>
     );
 }
