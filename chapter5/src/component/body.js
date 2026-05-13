b55399bca012beb7d234d19885bcb3a73fb81e87
문정훈 <mjh090801abc@gmail.com>
2026-05-13 15:02:29 +0900

    컴포넌트 변경에 따른 return값 변경

--- chapter5/src/component/body.js
+++ chapter5/src/component/body.js
@@ -1,10 +1,8 @@
-function Body({name, location, breed}) {
-    console.log(name, location, breed)
+function Body({children}) {
+    console.log(children)
     return (
         <div>
-            {name}은 {location}에 살고 있습니다.
-            <br></br>
-            {breed.length}개의 품종이 있습니다.
+            {children}
         </div>
     );
 
