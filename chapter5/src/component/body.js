f97390fc607ee20b9b6f846923e12532ad2ab911
문정훈 <mjh090801abc@gmail.com>
2026-05-14 11:45:57 +0900

    이름 / 성별 / 날짜 / 소개글을 포함한 웹페이지 작성

--- chapter5/src/component/body.js
+++ chapter5/src/component/body.js
@@ -1,14 +1,48 @@
-import { useState } from "react"
+import { use, useState } from "react"
 
 function Body() {
-    const [text, setText] = useState("")
-    const handleOnChange = (e) => {
-        setText(e.target.value)
+    
+    const [name, setName] = useState("")
+    const [gender, setGender] = useState("")
+    const [birth, setBirth] = useState("")
+    const [bio, setBio] = useState("")
+
+    const onChangeName = (e) => {
+        setName(e.target.value)
     }
+
+    const onChangeGender = (e) => {
+        setGender(e.target.value)
+    }
+
+    const onChangeBirth = (e) => {
+        setBirth(e.target.value)
+    }
+
+    const onChangeBio = (e) => {
+        setBio(e.target.value)
+    }
+
     return(
         <div>
-            <input value={text} onChange={handleOnChange} />
-            <div>입력한 글자 : "{text}" 입니노</div>
+            <div>
+                <div>
+                    <input value = {name} onChange={onChangeName} placeholder="이름" />
+                </div>
+            </div>
+            <div>
+                <select value={gender} onChange={onChangeGender}>
+                    <option>남성</option>
+                    <option>여성</option>
+                    <option>양성애자</option>
+                </select>
+            </div>
+            <div>
+                <input type="date" value={birth}onChange={onChangeBirth}></input>
+            </div>
+            <div>
+                <textarea value={bio} onChange={onChangeBio}></textarea>
+            </div>
         </div>
     )
 
