2b015e9ef2fc97b5f3336e704dcc6c966c8730a5
문정훈 <mjh090801abc@gmail.com>
2026-05-14 11:18:40 +0900

    증가 버튼을 만들고 화살표 함수를 사용하여서 숫자를 증가 시킬 수 있도록 코드 작성

--- chapter5/src/component/body.js
+++ chapter5/src/component/body.js
@@ -1,14 +1,30 @@
+import { useState } from "react"
+
 function Body() {
-    function handleOnClick(event) {
-        console.log(event)
-        console.log(event.target.name)
+    const [count, setCount] = useState(0)
+    const onIncrease = () => {
+        setCount(count + 1)
     }
-    return (
+    return(
         <div>
-            <button name="A버튼" onClick={handleOnClick}></button>
-            <button name="B버튼" onClick={handleOnClick}></button>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>증가
+            </button>
         </div>
-    );
+    )
+
+
+
+    // function handleOnClick(event) {
+    //     console.log(event)
+    //     console.log(event.target.name)
+    // }
+    // return (
+    //     <div>
+    //         <button name="A버튼" onClick={handleOnClick}></button>
+    //         <button name="B버튼" onClick={handleOnClick}></button>
+    //     </div>
+    // );
 
 }
 
