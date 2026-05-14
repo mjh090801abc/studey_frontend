542e2f62013606cbce117a522a401d245b44fba7
문정훈 <mjh090801abc@gmail.com>
2026-05-14 11:35:09 +0900

    text 박스 추가

--- chapter5/src/component/body.js
+++ chapter5/src/component/body.js
@@ -1,18 +1,16 @@
 import { useState } from "react"
 
 function Body() {
-    const [count, setCount] = useState(0)
-    const onIncrease = () => {
-        setCount(count + 1)
+    const [text, setText] = useState("")
+    const handleOnChange = (e) => {
+        setText(e.target.value)
     }
     return(
         <div>
-            <h2>{count}</h2>
-            <button onClick={onIncrease}>증가
-            </button>
+            <input value={text} onChange={handleOnChange} />
+            <div>입력한 글자 : "{text}" 입니노</div>
         </div>
     )
-
 
 
     // function handleOnClick(event) {
