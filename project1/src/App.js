c4df2633c35babc6172801f0a5d3dc234f4d73b9
문정훈 <mjh090801abc@gmail.com>
2026-05-21 11:35:58 +0900

    controller 코드 작성

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Controller from './component/Controller';
 
 function App() {
   return (
     <div className='App'>
       <h1>심플 카운터</h1>
+      <section>
+        <Viewer />
+        <Controller />
+      </section>
     </div>
-  
-
-
   );
 }
 
