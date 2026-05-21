841147aeff4a487a03548500f5e488640de5d3c9
문정훈 <mjh090801abc@gmail.com>
2026-05-21 12:00:52 +0900

    기능 추가 + css 컬러 수정

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Controller from './component/Controller';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0)
+  const handleSetCount = (value) => {
+    setCount(count + value)
+  }
   return (
     <div className='App'>
       <h1>심플 카운터</h1>
       <section>
-        <Viewer />
-        <Controller />
+        <Viewer count={count}/>
+      </section>
+      <section>
+        <controller handleSetCount={handleSetCount}/>
       </section>
     </div>
   );
