3aa430b41dc0217f137bd2d5d2855a3216ffb8bc
문정훈 <mjh090801abc@gmail.com>
2026-05-21 11:31:26 +0900

    카운터를 만들기 위한 코드 작성 세팅

---
+++ project1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>심플 카운터</h1>
+    </div>
+  
+
+
+  );
+}
+
+export default App;
---
+++ project1/src/component/Viewer.js
@@ -0,0 +1,7 @@
+const Viewer = () => {
+    return(
+        <div>
+            <div>현재카운트</div>
+        </div>
+    )
+}
